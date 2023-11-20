b42d887453642761be21ac5fb1114d739ac20567
Suphakit P <me@suphakit.net>
2023-11-20 20:52:39 +0700

    feat<module>: password encryption

--- src/modules/apis/users/createUser.js
+++ src/modules/apis/users/createUser.js
@@ -1,12 +1,47 @@
+const userSchemas = require('../../mongo/schemas/user')
+
 const UserCreationPage = require('../../../pages/backend/createUser')
+const { encryptData } = require('../../encryption')
 
 async function CreateUsers(app) {
   app.get('/backend/users/create', (req, res) => {
     res.send(UserCreationPage)
   })
 
-  app.post('/api/users/create', (req, res) => {
+  app.post('/api/users/create', async (req, res) => {
     const body = req.body
+
+    if (body['conf-password'] == body.password) {
+      console.log('password match')
+
+      const search = await userSchemas
+        .findOne({ username: body.username })
+        .exec()
+
+      if (search == null || search == undefined) {
+        let data = {
+          username: body.username,
+          password: encryptData(body.password),
+        }
+
+        let model = new userSchemas(data)
+
+        try {
+          await model.save()
+          res.redirect('/backend/users/create?status=1&message=user+created')
+        } catch (err) {
+          res.redirect('/backend/users/create?status=0&message=err')
+        }
+      } else {
+        console.log('username exist')
+        res.redirect('/backend/users/create?status=0&message=username+exist')
+      }
+    } else {
+      console.log('password field is not match')
+      res.redirect(
+        '/backend/users/create?status=0&message=confirm+password+field+is+not+match+password+field'
+      )
+    }
   })
 }
 
--- src/pages/backend/createUser.js
+++ src/pages/backend/createUser.js
@@ -1,42 +1,75 @@
 const UserCreationPage = `<!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Create new user | JentaJuice</title>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Create new user | JentaJuice</title>
 
-  <link rel="stylesheet" href="/statics/css/main.css">
-  <link rel="stylesheet" href="/statics/css/pages/users.css">
-</head>
-<body>
-  <div class="container">
-    <div class="form-container">
-      <form action="/api/create/user" method="post">
-        <input type="text" name="username" id="username" placeholder="Username" />
-        <input type="password" name="password" id="password" placeholder="Password">
-        <input type="password" name="conf-password" id="conf-password" placeholder="Confirm password">
-  
-        <button type="submit" id="submitButton" disabled>
-          Add user!
-        </button>
-      </form>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js"></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="/statics/css/main.css" />
+    <link rel="stylesheet" href="/statics/css/pages/users.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="form-container">
+        <form action="/api/users/create" method="post">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            name="conf-password"
+            id="conf-password"
+            placeholder="Confirm password"
+          />
+
+          <button type="submit" id="submitButton" disabled>Add user!</button>
+        </form>
+      </div>
     </div>
-  </div>
 
-  <script>
-    const password = document.querySelector('#password')
-    const confPassword = document.querySelector('#conf-password')
-    const button = document.querySelector('#submitButton')
-    
-    let x = setInterval(() => {
-      if (password.value == confPassword.value) {
-        button.disabled = false
-      } else {
-        button.disabled = true
+    <script>
+      const params = new URLSearchParams(window.location.search);
+      const statusParam = params.get("status");
+
+      if (statusParam !== undefined) {
+        if (statusParam == '1') {
+          Swal.fire("Success!", "User created!", "success");
+        } else {
+          Swal.fire("Error!", params.get('message'), "error");
+        }
       }
-    }, 1000)
-  </script>
-</body>
-</html>`
+    </script>
+
+    <script>
+      const password = document.querySelector("#password");
+      const confPassword = document.querySelector("#conf-password");
+      const button = document.querySelector("#submitButton");
+
+      let x = setInterval(() => {
+        if (password.value == confPassword.value) {
+          button.disabled = false;
+        } else {
+          button.disabled = true;
+        }
+      }, 1000);
+    </script>
+  </body>
+</html>
+`
 
 module.exports = UserCreationPage
