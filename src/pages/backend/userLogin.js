fadfae8f5fd6d7e66f28fa4b966ed12f3dd6670a
Suphakit P <me@suphakit.net>
2023-11-21 02:55:03 +0700

    feat<backend>: login page and style

---
+++ src/pages/backend/userLogin.js
@@ -0,0 +1,57 @@
+const LoginPage = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login | JentaJuice</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js"></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="/statics/css/main.css" />
+    <link rel="stylesheet" href="/statics/css/pages/users.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="form-container">
+        <form action="/api/users/login" method="post">
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            id="username"
+            required
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            id="password"
+            required
+          />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+
+    <script>
+      const params = new URLSearchParams(window.location.search);
+      const statusParam = params.get("status");
+
+      if (statusParam !== undefined) {
+        if (statusParam == "1") {
+          Swal.fire("Success!", "wtf why login and show here!", "success");
+        } else {
+          Swal.fire("Error!", params.get("message"), "error");
+        }
+      }
+    </script>
+  </body>
+</html>
+`
+
+module.exports = LoginPage
