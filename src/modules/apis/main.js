79319a0fde2aeca6713d2f7b08cd318e876d203e
Suphakit P <me@suphakit.net>
2023-11-22 00:31:48 +0700

    feat<backend>: login module

--- src/modules/apis/main.js
+++ src/modules/apis/main.js
@@ -1,7 +1,17 @@
 const CreateUsers = require('./users/createUser')
+const userLogin = require('./users/userLogin')
 
 async function MainAPI(app) {
   await CreateUsers(app)
+  await userLogin(app)
+
+  app.get('/backend', async (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/backend/dashboard')
+    } else {
+      res.redirect('/backend/login')
+    }
+  })
 }
 
 module.exports = MainAPI
--- src/modules/apis/users/userLogin.js
+++ src/modules/apis/users/userLogin.js
@@ -1,5 +1,43 @@
+const LoginPage = require('../../../pages/backend/userLogin')
+const { decryptData, encryptData } = require('../../encryption')
+const userSchema = require('../../mongo/schemas/user')
+
 async function userLogin(app) {
-  app.get('/api/login', (req, res) => {})
+  app.get('/backend/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/backend/dashboard')
+    } else {
+      res.send(LoginPage)
+    }
+  })
+
+  app.post('/api/users/login', async (req, res) => {
+    const body = req.body
+
+    if (body != undefined || body != {} || body != null) {
+      const user = await userSchema.findOne({
+        username: body.username,
+      })
+
+      if (user != undefined || user != null) {
+        let dec = decryptData(user.password)
+
+        if (dec == body.password) {
+          res.redirect('/backend/dashboard')
+        } else {
+          res.redirect(
+            '/backend/login?status=0&message=username+or+password+is+wrong'
+          )
+        }
+      } else {
+        res.redirect(
+          '/backend/login?status=0&message=username+or+password+is+wrong'
+        )
+      }
+    } else {
+      res.redirect('/backend/login?status=0&message=blank+body')
+    }
+  })
 }
 
 module.exports = userLogin
