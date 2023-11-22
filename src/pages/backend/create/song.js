485c2f68eabb8cef2b0706a2cd0832daf787b14f
Suphakit P <me@suphakit.net>
2023-11-22 15:20:35 +0700

    feat<backend>: added callback function

--- src/pages/backend/create/song.js
+++ src/pages/backend/create/song.js
@@ -20,36 +20,83 @@ async function artistName() {
 async function songsPage() {
   const page = `<!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Songs | JentaJuice</title>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Songs | JentaJuice</title>
 
-  <link rel="stylesheet" href="/statics/css/main.css">
-  <link rel="stylesheet" href="/statics/css/pages/users.css">
-</head>
-<body>
-  <div class="container">
-    <div class="form-container">
-      <form action="/api/songs/title" method="post">
-        <input type="text" name="title" id="title" placeholder="Song title" required>
-        <select name="artist" id="artist" required>
-          <option value="" selected>-- Select Artist --</option>
-          ${await artistName()}
-        </select>
-        <input type="number" name="year" id="year" placeholder="Copyright Year" required>
-        <input type="text" name="holder" id="holder" placeholder="Copyright Holder" required>
-        <input type="text" name="image" id="image" placeholder="Image URL" required>
-        <input type="date" name="releasedDate" id="releasedDate" placeholder="Released Date" required>
-        
-        <button type="submit">
-          Add!
-        </button>
-      </form>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js"></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="/statics/css/main.css" />
+    <link rel="stylesheet" href="/statics/css/pages/users.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="form-container">
+        <form action="/api/songs/title" method="post">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Song title"
+            required
+          />
+          <select name="artist" id="artist" required>
+            <option value="" selected>-- Select Artist --</option>
+            ${await artistName()}
+          </select>
+          <input
+            type="number"
+            name="year"
+            id="year"
+            placeholder="Copyright Year"
+            required
+          />
+          <input
+            type="text"
+            name="holder"
+            id="holder"
+            placeholder="Copyright Holder"
+            required
+          />
+          <input
+            type="text"
+            name="image"
+            id="image"
+            placeholder="Image URL"
+            required
+          />
+          <input
+            type="date"
+            name="releasedDate"
+            id="releasedDate"
+            placeholder="Released Date"
+            required
+          />
+
+          <button type="submit">Add!</button>
+        </form>
+      </div>
     </div>
-  </div>
-</body>
-</html>`
+    <script>
+      const params = new URLSearchParams(window.location.search);
+      const statusParam = params.get("status");
+
+      if (statusParam !== undefined) {
+        if (statusParam == "1") {
+          Swal.fire("Success!", params.get("message"), "success");
+        } else {
+          Swal.fire("Error!", params.get("message"), "error");
+        }
+      }
+    </script>
+  </body>
+</html>
+`
 
   return page
 }
