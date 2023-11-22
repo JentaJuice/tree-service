58f478b0471ee6988258ca5560da071281529bea
Suphakit P <me@suphakit.net>
2023-11-22 15:40:26 +0700

    feat<backend>: added route protection

--- src/modules/apis/users/createUser.js
+++ src/modules/apis/users/createUser.js
@@ -5,42 +5,50 @@ const { encryptData } = require('../../encryption')
 
 async function CreateUsers(app) {
   app.get('/backend/users/create', (req, res) => {
-    res.send(UserCreationPage)
+    if (req.session.loggedIn) {
+      res.send(UserCreationPage)
+    } else {
+      res.redirect('/backend/login')
+    }
   })
 
   app.post('/api/users/create', async (req, res) => {
     const body = req.body
 
-    if (body['conf-password'] == body.password) {
-      console.log('password match')
+    if (req.session.loggedIn) {
+      if (body['conf-password'] == body.password) {
+        console.log('password match')
 
-      const search = await userSchemas
-        .findOne({ username: body.username })
-        .exec()
+        const search = await userSchemas
+          .findOne({ username: body.username })
+          .exec()
 
-      if (search == null || search == undefined) {
-        let data = {
-          username: body.username,
-          password: encryptData(body.password),
-        }
+        if (search == null || search == undefined) {
+          let data = {
+            username: body.username,
+            password: encryptData(body.password),
+          }
 
-        let model = new userSchemas(data)
+          let model = new userSchemas(data)
 
-        try {
-          await model.save()
-          res.redirect('/backend/users/create?status=1&message=user+created')
-        } catch (err) {
-          res.redirect('/backend/users/create?status=0&message=err')
+          try {
+            await model.save()
+            res.redirect('/backend/users/create?status=1&message=user+created')
+          } catch (err) {
+            res.redirect('/backend/users/create?status=0&message=err')
+          }
+        } else {
+          console.log('username exist')
+          res.redirect('/backend/users/create?status=0&message=username+exist')
         }
       } else {
-        console.log('username exist')
-        res.redirect('/backend/users/create?status=0&message=username+exist')
+        console.log('password field is not match')
+        res.redirect(
+          '/backend/users/create?status=0&message=confirm+password+field+is+not+match+password+field'
+        )
       }
     } else {
-      console.log('password field is not match')
-      res.redirect(
-        '/backend/users/create?status=0&message=confirm+password+field+is+not+match+password+field'
-      )
+      res.redirect('/backend/login')
     }
   })
 }
