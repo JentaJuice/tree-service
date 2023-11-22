600581e6db08584bf0fb39e13b52b55638c5dc64
Suphakit P <me@suphakit.net>
2023-11-22 11:49:36 +0700

    feat<apis>: artist creation api

---
+++ src/modules/apis/songs/newArtist.js
@@ -0,0 +1,48 @@
+const artistSchemas = require('../../mongo/schemas/slug')
+const artistPage = require('../../../pages/backend/create/artist')
+
+async function newArtist(app) {
+  app.get('/backend/artists/create', async (req, res) => {
+    if (req.session.loggedIn) {
+      res.send(artistPage)
+    } else {
+      res.redirect('/backend/login')
+    }
+  })
+
+  app.post('/api/artists/create', async (req, res) => {
+    const body = req.body
+
+    if (req.session.loggedIn) {
+      if (body != undefined || body != null || body != {}) {
+        const check = await artistSchemas.findOne({ slug: body.slug })
+
+        if (check != undefined || check != null) {
+          res.redirect('/backend/artists/create?status=0&message=exist+slug')
+        } else {
+          let data = {
+            artist: body.artist,
+            slug: body.slug,
+          }
+
+          let artist = new artistSchemas(data)
+
+          try {
+            await artist.save()
+            res.redirect(
+              '/backend/artists/create?status=1&message=artist+create'
+            )
+          } catch (err) {
+            res.redirect('/backend/artists/create?status=0&message=' + err)
+          }
+        }
+      } else {
+        res.redirect('/backend/artists/create?status=0&message=blank+body')
+      }
+    } else {
+      res.redirect('/backend/login')
+    }
+  })
+}
+
+module.exports = newArtist
--- src/pages/backend/create/artist.js
+++ src/pages/backend/create/artist.js
@@ -5,22 +5,51 @@ const artistPage = `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Artists | JentaJuice</title>
 
-    <link rel="stylesheet" href="/statics/css/main.css">
-    <link rel="stylesheet" href="/statics/css/pages/users.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js"></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="/statics/css/main.css" />
+    <link rel="stylesheet" href="/statics/css/pages/users.css" />
   </head>
   <body>
     <div class="container">
       <div class="form-container">
-        <form action="/api/artists/create">
-          <input type="text" id="artist" name="artist" placeholder="Artist Name" required>
-          <input type="text" id="slug" name="slug" placeholder="Slug on URL" required>
+        <form action="/api/artists/create" method="post">
+          <input
+            type="text"
+            id="artist"
+            name="artist"
+            placeholder="Artist Name"
+            required
+          />
+          <input
+            type="text"
+            id="slug"
+            name="slug"
+            placeholder="Slug on URL"
+            required
+          />
 
-          <button type="submit">
-            Create!
-          </button>
+          <button type="submit">Create!</button>
         </form>
       </div>
     </div>
+
+    <script>
+      const params = new URLSearchParams(window.location.search);
+      const statusParam = params.get("status");
+
+      if (statusParam !== undefined) {
+        if (statusParam == "1") {
+          Swal.fire("Success!", params.get("message"), "success");
+        } else {
+          Swal.fire("Error!", params.get("message"), "error");
+        }
+      }
+    </script>
   </body>
 </html>
 `
