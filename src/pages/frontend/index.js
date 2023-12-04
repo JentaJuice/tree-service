fa622367d8d4824e43c74ea7d954dd5d2fe5ba41
Suphakit P <me@suphakit.net>
2023-12-04 10:31:33 +0700

    feat<archived>: index page

---
+++ src/pages/frontend/index.js
@@ -0,0 +1,30 @@
+async function indexPage() {
+  const Index = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JentaJuice</title>
+
+    <link rel="stylesheet" href="/statics/css/main.css">
+    <link rel="stylesheet" href="/statics/css/index.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="sub-container header top-header">
+        <h1>
+          JentaJuice
+        </h1>
+        <h3>
+          idk how i'm here :3
+        </h3>
+      </div>
+    </div>
+  </body>
+</html>
+`
+
+  return Index
+}
+
+module.exports = indexPage
--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,6 @@ const FrontEndRoutes = require('./modules/routes/main')
 
 async function Routes(app, exp, root) {
   await MainAPI(app)
-
-  app.get('/', (req, res) => {
-    res.send('hi :)')
-  })
-
-  // Mock
-  app.get('/mock/login', (req, res) => {
-    req.session.loggedIn = true
-    console.log('logged In')
-    res.send('logged In')
-  })
-
   await FrontEndRoutes(app)
 }
 
